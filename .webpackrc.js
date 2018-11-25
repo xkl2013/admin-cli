4096bf2f4bb1738d33521a64c0c039b4ccfa1716
mzh <manzhenhua@sunlands.com>
2018-11-25 12:42:36 +0800

    修改配置

---
+++ .webpackrc.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+export default {
+  entry: 'src/index.js',
+  extraBabelPlugins: [['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }]],
+  env: {
+    development: {
+      extraBabelPlugins: ['dva-hmr'],
+    },
+  },
+  alias: {
+    components: path.resolve(__dirname, 'src/components/'),
+    selfComponent: path.resolve(__dirname, 'src/selfComponent/'),
+    utils: path.resolve(__dirname, 'src/utils/'),
+  },
+  ignoreMomentLocale: true,
+  theme: './src/theme.js',
+  disableDynamicImport: false,
+  publicPath: '/',
+  hash: true,
+  define: {
+    "process.env.TEST": 1,
+    "USE_COMMA": 2,
+  }
+};
+
--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { connect } from 'dva';
+import {Button} from 'antd';
 import styles from './IndexPage.css';
 
 function IndexPage() {
+  console.log(process.env.TEST)
   return (
     <div className={styles.normal}>
       <h1 className={styles.title}>Yay! Welcome to dva!</h1>
       <div className={styles.welcome} />
+      <Button type="primary">点击</Button>
       <ul className={styles.list}>
         <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
         <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
